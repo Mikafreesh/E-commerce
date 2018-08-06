000add72c2c28979f011ce4b27dedab455885454
Mikafreesh <39375052+Mikafreesh@users.noreply.github.com>
2018-08-06 11:19:40 +0200

    Add files via upload
    Only php

---
+++ fabrication.view.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>BioEssence</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+         
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Knewave&amp;subset=latin-ext" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Gaegu" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Patrick+Hand" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/fabricationecom.css">
+        
+        
+    </head>
+
+    
+    <body>
+    <!--Debut NAV BAR-->
+ 
+<nav class="navbar navbar-expand-lg navbar-default">
+  <div class="navbar-brand logo"><img src="../image/logobio.png" alt="logobio" width=120 height=80> </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav menu">
+       <a class="nav-item nav-link" href="#"><i class="fas fa-home"></i> Accueil</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-braille"></i> Notre fabrication</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-envelope"></i> Contact</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-user"></i> Connexion</a>
+      <a class="nav-item nav-link" href="#"><i class="fas fa-shopping-cart"></i> Panier </a>
+    </div>
+  </div>
+
+</nav>
+ 
+   
+    <!-- Fin NAV BAR-->     
+    
+    <!-- DEBUT RUBRIQUE FABRICATION-->
+                <div class="container-fluid first">
+                            <div class="row">
+                            <div  class="col-lg-6 unoimage"> <img src="image/fabrication.jpg" alt="fabrication"  width=750 height=450> </div>
+                            <div  class="col-lg-6 unopara ">
+                            <h1> Le labo </h1> 
+                            <p> C’est au laboratoire <strong> "Bioessence" </strong> que nos produits sont confectionnés avec le plus grand soins afin de satisfaire notre client&egrave;le fid&egrave;le et grandissante. 
+                            </p>
+                            </div>
+                            </div>
+                    
+                     <div class="row">
+                    <div class="col-lg-6 dospara"> 
+                        <h1> Fabrication</h1> 
+                        <p> Nos huiles essentielles sont obtenues par distillation &agrave; la vapeur d’eau, sans détartrant chimique et sous basse pression.<br>
+                            C'est le résultat d'un travail minutieux et d'un savoir faire <br> anc&ecirc;strale.
+                        </p>
+                    </div>
+                    <div class="col-lg-6 dosimage"> <img src="../image/photo10.jpg" alt="preparation" width=850 height=450> </div>
+                    
+                </div>
+                </div>
+                
+                <div class="container-fluid twice">
+                    <div class=" row fabricone">
+       
+                        <div class="col-lg-4 plantation"> <span class="nombre"> 1 </span> <span class="titrefabric">  PLANTATION </span>
+                            <div class="pspecial"> La plantation se fait de façon très sélective <br>et manuelle. </div> 
+                        </div>    
+                        <div class="col-lg-4 floraison"> <span class="nombre"> 2 </span> <span class="titrefabric">  FLORAISON </span>
+                            <div class="pspecial"> Une floraison surveillée et suivie. </div> 
+                        </div>
+                        <div class="col-lg-4 recolte"> <span class="nombre"> 3 </span> <span class="titrefabric"> RECOLTE </span>
+                                <div class="pspecial"> La r&eacute;colte se fait manuellement et demande <br> beaucoup d’attention afin de ne pas abimer <br> la fleur.</div> 
+                        </div>
+                    </div>
+                    
+                    <div class="row fabrictwo">
+                        
+                        <div class="col-lg-4  distillerie"> <span class="nombre"> 4 </span> <span class="titrefabric"> DISTILLERIE  </span>
+                                <div class="pspecial"> La pr&eacute;paration de la récolte avant la distillerie.
+                                </div> 
+                            </div>
+                        <div class="col-lg-4  conditionnement"> <span class="nombre"> 5 </span> <span class="titrefabric"> CONDITIONNEMENT </span>
+                                <div class="pspecial"> Nos produits sont ensuite emballés et conditionn&eacute;s <br> avec le plus grands soins.
+                                </div>
+                            </div>
+                        <div class="col-lg-4  mise en vente"> <span class="nombre"> 6 </span> <span class="titrefabric"> MISE EN VENTE </span>
+                                <div class="pspecial"> Les produits sont ensuite mis en magasins <br> et envoyés  &agrave; nos distributeurs et clients.</div>
+                            </div>
+                    
+                   </div>
+                </div>
+     <!-- FIN  RUBRIQUE FABRICATION-->              
+    </body>
+</html>
